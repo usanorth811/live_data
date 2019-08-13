6807dd777f9bb5e2b8a77a12e6364f1759c5c817
Caleb Woods <woodscaleb@gmail.com>
2019-08-13 12:41:37 -0700

    added ca and nv ticket counts

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,27 @@
   <div id="app">
     <b-container>
       <b-row>
-        <b-col md='3' offset='1'>
+        <b-col lg='4' md='12' >
           <div>
-            <h4>Today's Tickets</h4>
+            <h4>Total Tickets</h4>
             <b-table striped hover :items="info"></b-table>
           </div>
-        </b-col>
-        <b-col md='3' >
           <div>
-            <h4>DPS Leaderboard</h4>
-            <b-table striped hover :items="info2"></b-table>
+            <h4>CA Tickets</h4>
+            <b-table striped hover :items="info4"></b-table>
+          </div>
+          <div>
+            <h4>NV Tickets</h4>
+            <b-table striped hover :items="info5"></b-table>
           </div>
         </b-col>
-        <b-col md='3' >
+        <b-col lg='4' md='12' style="position:relative; overflow-y:auto; height:565px;">
+          <div>
+            <h4>DPS Leaderboard</h4>
+            <b-table striped hover :items="info2" ></b-table>
+          </div>
+        </b-col>
+        <b-col lg='4' md='12' style="position:relative; overflow-y:auto; height:565px;">
           <div>
             <h4>WEB Leaderboard</h4>
             <b-table striped hover :items="info3"></b-table>
@@ -32,16 +40,26 @@ export default {
     return {
       info: null,
       info2: null,
-      info3: null
+      info3: null,
+      info4: null,
+      info5: null
     }
   },
   mounted () {
+    axios.get('https://guarded-depths.com/reports/total')
+        .then(response => (self.info = response.data.Referral), axios.get('https://guarded-depths.com/reports/dps')
+        .then(response => (self.info2 = response.data.Referral), axios.get('https://guarded-depths.com/reports/web')
+        .then(response => (self.info3 = response.data.Referral), axios.get('https://guarded-depths.com/reports/ca')
+        .then(response => (self.info4 = response.data.Referral), axios.get('https://guarded-depths.com/reports/nv')
+        .then(response => (self.info5 = response.data.Referral))))));
     var self = this;
     setInterval(function(){
       axios.get('https://guarded-depths.com/reports/total')
         .then(response => (self.info = response.data.Referral), axios.get('https://guarded-depths.com/reports/dps')
         .then(response => (self.info2 = response.data.Referral), axios.get('https://guarded-depths.com/reports/web')
-        .then(response => (self.info3 = response.data.Referral))));
+        .then(response => (self.info3 = response.data.Referral), axios.get('https://guarded-depths.com/reports/ca')
+        .then(response => (self.info4 = response.data.Referral), axios.get('https://guarded-depths.com/reports/nv')
+        .then(response => (self.info5 = response.data.Referral))))));
     }, 3000);
       
   }
