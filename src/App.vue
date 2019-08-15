66e011942b087debe80ff72d56f99da40c5a7394
Caleb Woods <woodscaleb@gmail.com>
2019-08-15 10:12:50 -0700

    added charts

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,8 @@
         <b-col cols='6'>
           <div style=' '>
           <h2>
-          Total Tickets: {{info[0].Total + info[1].Total}}  -  {{((info[0].Total/(info[0].Total + info[1].Total))*100).toString().slice(0,5)}}% Web </h2>
-          <h4>Express Tickets : {{info8[0].Total}}</h4>
+          Total Tickets: {{total[0].Total + total[1].Total}}  -  {{((total[0].Total/(total[0].Total + total[1].Total))*100).toString().slice(0,5)}}% Web </h2>
+          <h4>Express Tickets : {{sat[0].Total}}</h4>
           </div>
         </b-col>
       </b-row>
@@ -32,61 +32,95 @@
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
-      <b-carousel-slide img-blank img-alt="Blank image">
+
+    <b-carousel-slide id='slide_1' img-blank img-alt="Blank image">
+      <h3>Year to Date</h3>
+        <b-row style='padding-bottom:50px;height: 550px'>
+        <b-col md='12' >
+          <div>
+            <h4>{{(ytd[0].Total + ytd[1].Total).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}} Tickets - {{((ytd[0].Total/(ytd[0].Total + ytd[1].Total))*100).toString().slice(0,5)}}% Web</h4>
+            <b-table striped hover dark :items="ytd"></b-table>
+          </div>
+        </b-col>
+        </b-row>
+        <b-row>
+        <b-col  md='12' style='margin-top:-350px'>
+          <column-chart height='250px' :data="[[month[0].Name, month[0].Total], [month[1].Name, month[1].Total],[month[2].Name, month[2].Total],[month[3].Name, month[3].Total], [month[4].Name, month[4].Total],[month[5].Name, month[5].Total],[month[6].Name, month[6].Total], [month[7].Name, month[7].Total]]"></column-chart>
+        </b-col>
+      </b-row>
+      </b-carousel-slide>
+
+
+      <b-carousel-slide id='slide_2' img-blank img-alt="Blank image">
+        <h3>Live Feed</h3>
+        <b-row style='padding-bottom:25px;height: 600px'>
+        <b-col md='4' >
+          <div>
+            <h4>Total Tickets - {{((total[0].Total/(total[0].Total + total[1].Total))*100).toString().slice(0,5)}}% Web</h4>
+            <b-table striped hover dark :items="total"></b-table>
+          </div>
+          <pie-chart width="auto" height="250px" :data="[[total[0].Name, total[0].Total], [total[1].Name, total[1].Total]]" :colors="['#42b983', '#e83e8c']" />
+        </b-col>
+        <b-col md='8' >
+          <div>
+            <h5>CA Tickets {{((ca[0].Total/(ca[0].Total + ca[1].Total))*100).toString().slice(0,5)}}% Web</h5>
+            <b-table striped hover  dark :items="ca"></b-table>
+          </div>
+          <div>
+            <h5>NV Tickets {{((nv[0].Total/(nv[0].Total + nv[1].Total))*100).toString().slice(0,5)}}% Web</h5>
+            <b-table striped hover  dark :items="nv"></b-table>
+          </div>
+        </b-col>
+        </b-row>
+        
+      </b-carousel-slide>
+
+      <b-carousel-slide id='slide_3' img-blank img-alt="Blank image">
+        
         <b-row style='padding-bottom:25px;position:relative; overflow-y:auto;height: 600px'>
         <b-col lg='4' md='12' >
-          <div>
-            <h4>Total Tickets {{((info[0].Total/(info[0].Total + info[1].Total))*100).toString().slice(0,5)}}% Web</h4>
-            <b-table striped hover dark :items="info"></b-table>
-          </div>
-          <div>
-            <h4>CA Tickets {{((info4[0].Total/(info4[0].Total + info4[1].Total))*100).toString().slice(0,5)}}% Web</h4>
-            <b-table striped hover dark :items="info4"></b-table>
-          </div>
-          <div>
-            <h4>NV Tickets {{((info5[0].Total/(info5[0].Total + info5[1].Total))*100).toString().slice(0,5)}}% Web</h4>
-            <b-table striped hover dark :items="info5"></b-table>
-          </div>
+          <h4>Web Tickets</h4>
+        <pie-chart width="auto" height="250px" :data="[[type_web[0].Name, type_web[0].Total], [type_web[1].Name, type_web[1].Total],[type_web[2].Name, type_web[2].Total]]" :colors="['#42b983', '#ffc107', '#e83e8c']" />
+        <h4>DPS Tickets</h4>
+        <pie-chart width="auto" height="250px" :data="[[type_csr[0].Name, type_csr[0].Total], [type_csr[1].Name, type_csr[1].Total],[type_csr[2].Name, type_csr[2].Total]]" :colors="['#42b983', '#ffc107', '#e83e8c']" />
         </b-col>
         <b-col lg='8' md='12' style="position:relative; overflow-y:auto;">
           <div>
             <h4>Web %</h4>
             <h5 style="margin-bottom: -15px;">
-            Contractors: {{((info7[0].Total/(info7[0].Total + info6[0].Total))*100).toString().slice(0,5)}}% - 
-            Members: {{((info7[1].Total/(info7[1].Total + info6[1].Total))*100).toString().slice(0,5)}}% - 
-            Homeowners: {{((info7[2].Total/(info7[2].Total + info6[2].Total))*100).toString().slice(0,5)}}% - </h5>
-            <hr class="my-4" style="border-color:#ced4da; margin-left:175px;margin-right:175px;">
+            Contractors: {{((type_web[0].Total/(type_web[0].Total + type_csr[0].Total))*100).toString().slice(0,5)}}% - 
+            Members: {{((type_web[1].Total/(type_web[1].Total + type_csr[1].Total))*100).toString().slice(0,5)}}% - 
+            Homeowners: {{((type_web[2].Total/(type_web[2].Total + type_csr[2].Total))*100).toString().slice(0,5)}}% </h5>
+            <hr class="my-4" style="border-color:#ced4da; margin-left:100px;margin-right:100px;">
             
           </div>
           <div style=" margin-top: -15px">
-            <h4>Web Tickets by User Type</h4>
-            <b-table striped hover dark :items="info7" ></b-table>
+            <h4>Web Tickets </h4>
+            <b-table striped hover dark :items="type_web" ></b-table>
           </div>
         
           <div>
-            <h4>DPS Tickets by User Type</h4>
-            <b-table striped hover dark :items="info6"></b-table>
+            <h4>DPS Tickets </h4>
+            <b-table striped hover dark :items="type_csr"></b-table>
           </div>
         </b-col>
       </b-row>
       </b-carousel-slide>
 
-      
-
       <!-- Slides with img slot -->
       <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide img-blank img-alt="Blank image">
+      <b-carousel-slide id='slide_4' img-blank img-alt="Blank image">
         <b-row style='padding-bottom:25px;position:relative; overflow-y:auto;height: 600px'>
         <b-col lg='6' md='12' style="padding-bottom:25px">
           <div>
             <h4>DPS Leaderboard</h4>
-            <b-table striped hover dark :items="info2" ></b-table>
+            <b-table striped hover dark :items="dps" ></b-table>
           </div>
         </b-col>
         <b-col lg='6' md='12' style="padding-bottom:25px">
           <div>
             <h4>WEB Leaderboard</h4>
-            <b-table striped hover dark :items="info3"></b-table>
+            <b-table striped hover dark :items="web"></b-table>
           </div>
         </b-col>
       </b-row>
@@ -98,36 +132,36 @@
 <b-row style='padding-bottom:25px;'>
         <b-col lg='4' md='12' >
           <div>
-            <h4>Total Tickets {{((info[0].Total/(info[0].Total + info[1].Total))*100).toString().slice(0,5)}}% Web</h4>
-            <b-table striped hover dark :items="info"></b-table>
+            <h4>Total Tickets {{((total[0].Total/(total[0].Total + total[1].Total))*100).toString().slice(0,5)}}% Web</h4>
+            <b-table striped hover dark :items="total"></b-table>
           </div>
           <div>
-            <h4>CA Tickets {{((info4[0].Total/(info4[0].Total + info4[1].Total))*100).toString().slice(0,5)}}% Web</h4>
-            <b-table striped hover dark :items="info4"></b-table>
+            <h4>CA Tickets {{((ca[0].Total/(ca[0].Total + ca[1].Total))*100).toString().slice(0,5)}}% Web</h4>
+            <b-table striped hover dark :items="ca"></b-table>
           </div>
           <div>
-            <h4>NV Tickets {{((info5[0].Total/(info5[0].Total + info5[1].Total))*100).toString().slice(0,5)}}% Web</h4>
-            <b-table striped hover dark :items="info5"></b-table>
+            <h4>NV Tickets {{((nv[0].Total/(nv[0].Total + nv[1].Total))*100).toString().slice(0,5)}}% Web</h4>
+            <b-table striped hover dark :items="nv"></b-table>
           </div>
         </b-col>
         <b-col lg='8' md='12' style="position:relative; overflow-y:auto;">
           <div>
             <h4>Web %</h4>
             <h5 style="margin-bottom: -15px;">
-            Contractors: {{((info7[0].Total/(info7[0].Total + info6[0].Total))*100).toString().slice(0,5)}}% - 
-            Members: {{((info7[1].Total/(info7[1].Total + info6[1].Total))*100).toString().slice(0,5)}}% - 
-            Homeowners: {{((info7[2].Total/(info7[2].Total + info6[2].Total))*100).toString().slice(0,5)}}% - </h5>
+            Contractors: {{((type_web[0].Total/(type_web[0].Total + type_csr[0].Total))*100).toString().slice(0,5)}}% - 
+            Members: {{((type_web[1].Total/(type_web[1].Total + type_csr[1].Total))*100).toString().slice(0,5)}}% - 
+            Homeowners: {{((type_web[2].Total/(type_web[2].Total + type_csr[2].Total))*100).toString().slice(0,5)}}% - </h5>
             <hr class="my-4" style="border-color:#ced4da; margin-left:175px;margin-right:175px;">
             
           </div>
           <div style=" margin-top: -15px">
             <h4>Web Tickets by User Type</h4>
-            <b-table striped hover dark :items="info7" ></b-table>
+            <b-table striped hover dark :items="type_web" ></b-table>
           </div>
         
           <div>
             <h4>DPS Tickets by User Type</h4>
-            <b-table striped hover dark :items="info6"></b-table>
+            <b-table striped hover dark :items="type_csr"></b-table>
           </div>
         </b-col>
       </b-row>
@@ -135,13 +169,13 @@
         <b-col lg='6' md='12' style="position:relative; overflow-y:auto; height:580px;padding-bottom:25px">
           <div>
             <h4>DPS Leaderboard</h4>
-            <b-table striped hover dark :items="info2" ></b-table>
+            <b-table striped hover dark :items="dps" ></b-table>
           </div>
         </b-col>
         <b-col lg='6' md='12' style="padding-bottom:25px">
           <div>
             <h4>WEB Leaderboard</h4>
-            <b-table striped hover dark :items="info3"></b-table>
+            <b-table striped hover dark :items="web"></b-table>
           </div>
         </b-col>
       </b-row>
@@ -158,38 +192,44 @@ export default {
   name: 'app',
   data () {
     return {
-      info: null,
-      info2: null,
-      info3: null,
-      info4: null,
-      info5: null,
-      info6: null,
-      info7: null,
-      info8: null,
+      total: null,
+      dps: null,
+      web: null,
+      ca: null,
+      nv: null,
+      type_csr: null,
+      type_web: null,
+      sat: null,
+      month: null,
+      ytd: null,
       myDate : new Date().toISOString().slice(5,10)
     }
   },
   mounted () {
     axios.get('https://guarded-depths.com/reports/total')
-        .then(response => (self.info = response.data.Referral), axios.get('https://guarded-depths.com/reports/dps')
-        .then(response => (self.info2 = response.data.Referral), axios.get('https://guarded-depths.com/reports/web')
-        .then(response => (self.info3 = response.data.Referral), axios.get('https://guarded-depths.com/reports/ca')
-        .then(response => (self.info4 = response.data.Referral), axios.get('https://guarded-depths.com/reports/nv')
-        .then(response => (self.info5 = response.data.Referral), axios.get('https://guarded-depths.com/reports/type_csr')
-        .then(response => (self.info6 = response.data.Referral), axios.get('https://guarded-depths.com/reports/type_web')
-        .then(response => (self.info7 = response.data.Referral), axios.get('https://guarded-depths.com/reports/sat')
-        .then(response => (self.info8 = response.data.Referral)))))))));
+        .then(response => (self.total = response.data.Referral), axios.get('https://guarded-depths.com/reports/dps')
+        .then(response => (self.dps = response.data.Referral), axios.get('https://guarded-depths.com/reports/web')
+        .then(response => (self.web = response.data.Referral), axios.get('https://guarded-depths.com/reports/ca')
+        .then(response => (self.ca = response.data.Referral), axios.get('https://guarded-depths.com/reports/nv')
+        .then(response => (self.nv = response.data.Referral), axios.get('https://guarded-depths.com/reports/type_csr')
+        .then(response => (self.type_csr = response.data.Referral), axios.get('https://guarded-depths.com/reports/type_web')
+        .then(response => (self.type_web = response.data.Referral), axios.get('https://guarded-depths.com/reports/sat')
+        .then(response => (self.sat = response.data.Referral), axios.get('https://guarded-depths.com/reports/month')
+        .then(response => (self.month = response.data.Referral), axios.get('https://guarded-depths.com/reports/ytd')
+        .then(response => (self.ytd = response.data.Referral)))))))))));
     var self = this;
     setInterval(function(){
       axios.get('https://guarded-depths.com/reports/total')
-        .then(response => (self.info = response.data.Referral), axios.get('https://guarded-depths.com/reports/dps')
-        .then(response => (self.info2 = response.data.Referral), axios.get('https://guarded-depths.com/reports/web')
-        .then(response => (self.info3 = response.data.Referral), axios.get('https://guarded-depths.com/reports/ca')
-        .then(response => (self.info4 = response.data.Referral), axios.get('https://guarded-depths.com/reports/nv')
-        .then(response => (self.info5 = response.data.Referral), axios.get('https://guarded-depths.com/reports/type_csr')
-        .then(response => (self.info6 = response.data.Referral), axios.get('https://guarded-depths.com/reports/type_web')
-        .then(response => (self.info7 = response.data.Referral), axios.get('https://guarded-depths.com/reports/sat')
-        .then(response => (self.info8 = response.data.Referral)))))))));
+        .then(response => (self.total = response.data.Referral), axios.get('https://guarded-depths.com/reports/dps')
+        .then(response => (self.dps = response.data.Referral), axios.get('https://guarded-depths.com/reports/web')
+        .then(response => (self.web = response.data.Referral), axios.get('https://guarded-depths.com/reports/ca')
+        .then(response => (self.ca = response.data.Referral), axios.get('https://guarded-depths.com/reports/nv')
+        .then(response => (self.nv = response.data.Referral), axios.get('https://guarded-depths.com/reports/type_csr')
+        .then(response => (self.type_csr = response.data.Referral), axios.get('https://guarded-depths.com/reports/type_web')
+        .then(response => (self.type_web = response.data.Referral), axios.get('https://guarded-depths.com/reports/sat')
+        .then(response => (self.sat = response.data.Referral), axios.get('https://guarded-depths.com/reports/month')
+        .then(response => (self.month = response.data.Referral), axios.get('https://guarded-depths.com/reports/ytd')
+        .then(response => (self.ytd = response.data.Referral)))))))))));
     }, 20000);
       
   }
--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
+import Chartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+
 
 Vue.use(BootstrapVue)
+Vue.use(Chartkick.use(Chart))
+
 
 new Vue({
   el: '#app',
